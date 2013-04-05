80ac6617eafb4a7620131f575419e568ef62a104
ajcubeta <ajcubeta@gmail.com>
2013-04-05 17:33:22 +0800

    Initialize files for booting rails 4 playground

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails4Playground::Application.config.secret_token = '953f1b25bdc28c3ce4e631aa0c1b753e17d7bd59de993c2858eba660577aa8bbef053d8f0ab7845cb1731c0ac5a4be6bea5f79b74fc769bd10d8079707139a56'
